1b0e172cf620006fd80e77f0fecf77281283ded1
Sergey Fokin <foks_serg@mail.ru>
2025-10-19 17:46:43 +0500

    feat: добавлено критическое правило безопасности проекта
    - Добавлено правило работы только в пределах текущей папки проекта
    - Запрещено заходить в корневую папку /Volumes/FSA-PRJ/Project/
    - Запрещено работать с соседними проектами (FSA-DateStamp/, FSA-PingChecker/, Install/)
    - Запрещено использовать абсолютные пути за пределами FSA-AstraInstall/
    - Всегда использовать относительные пути от корня проекта FSA-AstraInstall/
    - Принцип изоляции: ОДИН ПРОЕКТ = ОДНА ПАПКА
    - Контроль доступа: все команды только в FSA-AstraInstall/
    
    Измененные файлы:
    - ASSISTANT_RULES.md: добавлено критическое правило безопасности проекта

--- astra_automation.py
+++ astra_automation.py
@@ -6,12 +6,12 @@ from __future__ import print_function
 FSA-AstraInstall Automation - Единый исполняемый файл
 Автоматически распаковывает компоненты и запускает автоматизацию astra-setup.sh
 Совместимость: Python 3.x
-Версия: V2.2.65 (2025.10.16)
+Версия: V2.2.6566 (2025.10.16)
 Компания: ООО "НПА Вира-Реалтайм"
 """
 
 # Версия приложения
-APP_VERSION = "V2.2.65"
+APP_VERSION = "V2.2.6566"
 import os
 import sys
 import tempfile
